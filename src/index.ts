aa662dd33ae2ca4b2ac8163f7659510c27c1aea8
Hein-Jan Leliveld <hein-jan.leliveld@ns.nl>
2025-03-11 11:21:46 +0100

    Horizontal wins tests OK

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 export function checkWin(tictac: number[][]):  number {
-    if ((tictac[0][0] == tictac[0][1]) &&
+    if ((tictac[0][0] != 0) &&
+        (tictac[0][0] == tictac[0][1]) &&
         (tictac[0][0] == tictac[0][2])) {
             return tictac[0][0]
          }
+    if ((tictac[1][0] != 0) &&
+        (tictac[1][0] == tictac[1][1]) &&
+        (tictac[1][0] == tictac[1][2])) {
+            return tictac[1][0]
+    }     
+    if ((tictac[2][0] != 0) &&
+        (tictac[2][0] == tictac[2][1]) &&
+        (tictac[2][0] == tictac[2][2])) {
+            return tictac[2][0]
+    }     
     return 0
 }
