226637ab304a1a3cf59cd4c467412be6a62e6d93
Hein-Jan Leliveld <hein-jan.leliveld@ns.nl>
2025-03-11 11:18:07 +0100

    Second test succeeds

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export function checkWin(tictac: number[][]):  number {
+    if ((tictac[0][0] == tictac[0][1]) &&
+        (tictac[0][0] == tictac[0][2])) {
+            return tictac[0][0]
+         }
     return 0
 }
--- tests/test.ts
+++ tests/test.ts
@@ -1,16 +1,21 @@
-describe('emptyTest', () => {
-    it('should just succeed', () => {
-        const result = 1;
-    expect(result).toEqual(1);
-    });
-});
+import { checkWin } from '../src/index';
 
 describe('noWin', () => {
     it('should return 0: start array so nobody has won', () => {
         const tictac = [[0, 0, 0],
-                        [0, 0, 0], 
+                        [0, 0, 0],
                         [0, 0, 0]];
         const result = checkWin(tictac);
         expect(result).toEqual(0);
+    });
+  });
+
+describe('topRowWins', () => {
+    it('should return player 1 wins', () => {
+        const tictac = [[1, 1, 1],
+                        [0, 2, 0], 
+                        [2, 2, 0]];
+        const result = checkWin(tictac);
+        expect(result).toEqual(1);
     });
   });
