74bff5045f697e995920579e62ed222b7874ab92
Hein-Jan Leliveld <hein-jan.leliveld@ns.nl>
2025-03-11 11:28:51 +0100

    refactor row-check

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 export function checkWin(tictac: number[][]):  number {
-    if ((tictac[0][0] != 0) &&
-        (tictac[0][0] == tictac[0][1]) &&
-        (tictac[0][0] == tictac[0][2])) {
-            return tictac[0][0]
+    
+    for (var i = 0; i <3; i++) {
+        const row = tictac[i];
+        if ((row[0] != 0) &&
+            (row[0] == row[1]) &&
+            (row[0] == row[2])) {
+            return row[0]
          }
-    if ((tictac[1][0] != 0) &&
-        (tictac[1][0] == tictac[1][1]) &&
-        (tictac[1][0] == tictac[1][2])) {
-            return tictac[1][0]
-    }     
-    if ((tictac[2][0] != 0) &&
-        (tictac[2][0] == tictac[2][1]) &&
-        (tictac[2][0] == tictac[2][2])) {
-            return tictac[2][0]
-    }     
+    }
     return 0
 }
