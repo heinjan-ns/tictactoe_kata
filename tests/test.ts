2e503c5c9eb2893dcde99f9f746e4305b1d8eab0
Hein-Jan Leliveld <hein-jan.leliveld@ns.nl>
2025-03-11 10:57:32 +0100

    First test: empty tictac should return no winner

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,16 @@
 describe('emptyTest', () => {
-  it('should just succeed', () => {
-    const result = 1;
+    it('should just succeed', () => {
+        const result = 1;
     expect(result).toEqual(1);
-  });
+    });
 });
+
+describe('noWin', () => {
+    it('should return 0: start array so nobody has won', () => {
+        const tictac = [[0, 0, 0],
+                        [0, 0, 0], 
+                        [0, 0, 0]];
+        const result = checkWin(tictac);
+        expect(result).toEqual(0);
+    });
+  });
